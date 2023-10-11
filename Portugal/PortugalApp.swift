215b1e585fb1b3c99dc3efb56a8993873c20892c
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2023-10-11 15:02:58 +0400

    Initial Commit

---
+++ Portugal/PortugalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortugalApp.swift
+//  Portugal
+//
+//  Created by Tako Menteshashvili on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct PortugalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
